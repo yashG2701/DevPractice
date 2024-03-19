6898efdd5ca50cffb581d7c6662aa41bda526ff6
yashG2701 <98008007+yashG2701@users.noreply.github.com>
2024-03-19 09:53:25 +0530

    Update app.py

--- python/app.py
+++ python/app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+def index():
+    return app.send_static_file('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True)
